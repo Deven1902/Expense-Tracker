89c677e212c20b526b5b6cba898f92d2503a0d30
Deven1902 <devenamazingnandapurkar@gmail.com>
2023-05-18 10:02:11 +0530

    updated expses-filter__control

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -80,7 +80,7 @@ const ExpenseForm = (props) => {
           <input
             type='date'
             min='2019-01-01'
-            max='2030-12-31'
+            max='2050-12-31'
             value={enteredDate}
             onChange={dateChangeHandler}
           />
